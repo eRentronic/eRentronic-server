20456da937042a636531b92664e91799547988dc
hsjang <hsjang0405@gmail.com>
2022-07-20 23:13:23 +0900

    🎉 프로젝트 초기 세팅
    Co-Authored-By: riako <yhsep7@gmail.com>

---
+++ src/main/java/com/server/erentronic/ERentronicApplication.java
@@ -0,0 +1,13 @@
+package com.server.erentronic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERentronicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERentronicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/erentronic/ERentronicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.erentronic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERentronicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

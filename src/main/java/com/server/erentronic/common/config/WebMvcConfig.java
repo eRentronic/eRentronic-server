082141340c789601a1e845ffc35b5a9623eb201e
hsjang <hsjang0405@gmail.com>
2022-08-04 17:25:14 +0900

    ✨ url prefix /api 설정 config 추가
    Co-Authored-By: riako <yhsep7@gmail.com>

---
+++ src/main/java/com/server/erentronic/common/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.server.erentronic.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.addPathPrefix("/api",
+			HandlerTypePredicate.forBasePackage("com.server.erentronic"));
+	}
+}

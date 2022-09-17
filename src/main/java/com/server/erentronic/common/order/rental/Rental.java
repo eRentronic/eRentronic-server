bc5524d1c82ee91920375c2091716edfdc9a71f6
hsjang <hsjang0405@gmail.com>
2022-09-17 15:38:13 +0900

    ✨ 상품 대여 시 할인 정보, 할인 가격 검증 기능 추가
    Co-Authored-By: riako <yhsep7@gmail.com>

--- src/main/java/com/server/erentronic/common/order/rental/Rental.java
+++ src/main/java/com/server/erentronic/common/order/rental/Rental.java
@@ -1,9 +1,12 @@
 package com.server.erentronic.common.order.rental;
 
+import com.server.erentronic.common.discountpolicy.DiscountPolicy;
 import com.server.erentronic.common.exception.NotMatchException;
 import com.server.erentronic.common.message.ErrorDetail;
 import com.server.erentronic.common.order.Order;
+import com.server.erentronic.common.utils.SalePriceCalculator;
 import com.server.erentronic.item.product.Product;
+import com.server.erentronic.item.product.ProductDiscountPolicy;
 import com.server.erentronic.item.product.ProductRentalUnit;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -11,10 +14,14 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Rental extends Order {
 
 	@DateTimeFormat(iso = ISO.DATE_TIME)
@@ -29,22 +36,53 @@ public class Rental extends Order {
 	public static Rental makeRental(Product product, Integer orderQuantity, LocalDateTime startDateTime,
 		LocalDateTime endDateTime, Integer orderPrice) {
 
-		int rentalPeriod = (int) ChronoUnit.DAYS.between(startDateTime, endDateTime);
-
-		if (orderPrice.compareTo(product.getRentalPrice() * rentalPeriod * orderQuantity) != 0) {
-			throw new NotMatchException(ErrorDetail.NOT_EQUALS_REAL_PRICE);
-		}
-
 		product.decreaseRentalQuantity(orderQuantity);
 
-		Rental rental = new Rental();
-		rental.product = product;
-		rental.quantity = orderQuantity;
-		rental.price = orderPrice;
+		Rental rental = getRental(product, orderQuantity, orderPrice,
+			(int) ChronoUnit.DAYS.between(startDateTime, endDateTime));
+
 		rental.startDateTime = startDateTime;
 		rental.endDateTime = endDateTime;
 
+		validatePrice(orderPrice, rental);
+
 		return rental;
+	}
+
+	private static Rental getRental(Product product, Integer orderQuantity, Integer orderPrice, int rentalPeriod) {
+		Rental rental = new Rental();
+
+		iterateDiscountPolicies(product, rental);
+
+		rental.product = product;
+		rental.quantity = orderQuantity;
+		rental.price = orderPrice;
+		rental.salePrice = SalePriceCalculator.calculate(product.getRentalPrice() * rentalPeriod * orderQuantity,
+			rental.totalDiscountRate);
+
+
+		return rental;
+	}
+
+	private static void iterateDiscountPolicies(Product product, Rental rental) {
+		StringBuilder discountDetailBuilder = new StringBuilder();
+
+
+		rental.totalDiscountRate = 0.0;
+
+		for (ProductDiscountPolicy productDiscountPolicy : product.getDiscountPolicies()) {
+			DiscountPolicy discountPolicy = productDiscountPolicy.getDiscountPolicy();
+			rental.totalDiscountRate += discountPolicy.getRate();
+			discountDetailBuilder.append(discountPolicy).append(System.lineSeparator());
+		}
+		rental.discountDetail = discountDetailBuilder.toString();
+
+	}
+
+	private static void validatePrice(Integer orderPrice, Rental rental) {
+		if (orderPrice.compareTo(rental.salePrice) != 0) {
+			throw new NotMatchException(ErrorDetail.NOT_EQUALS_REAL_PRICE);
+		}
 	}
 
 	public void assignUnits(List<ProductRentalUnit> units) {

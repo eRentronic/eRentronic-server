c4a38875dd9675956d6774077342380d7e2792be
hsjang <hsjang0405@gmail.com>
2022-10-20 21:23:25 +0900

    🐛 클라이언트가 특정 헤더 값에 대해 접근할 수 있도록 설정
    Co-Authored-By: riako <yhsep7@gmail.com>

--- src/main/java/com/server/erentronic/auth/AuthConst.java
+++ src/main/java/com/server/erentronic/auth/AuthConst.java
@@ -8,5 +8,6 @@ public class AuthConst {
 
 	public static final String MEMBER_ID = "memberId";
 	public static final String ACCESS_TOKEN = "Access-Token";
+	public static final String AUTHORIZATION = "Authorization";
 	public static final String BEARER = "bearer";
 }

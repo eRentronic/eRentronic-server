e06fea32579fac5e044a164708803bc02187be46
naneun <rmtlrksdp@naver.com>
2022-11-03 23:41:38 +0900

    ♻ allowedMethods 직접 명시
    Co-Authored-By: jeremy0405 <hsjang0405@gmail.com>

--- src/main/java/com/server/erentronic/common/config/WebMvcConfig.java
+++ src/main/java/com/server/erentronic/common/config/WebMvcConfig.java
@@ -29,9 +29,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
-			.exposedHeaders(AuthConst.ACCESS_TOKEN, AuthConst.AUTHORIZATION)
-			.allowCredentials(true);
+				.allowedOrigins("*")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+				.exposedHeaders(AuthConst.ACCESS_TOKEN, AuthConst.AUTHORIZATION);
 	}
 
 	@Override
